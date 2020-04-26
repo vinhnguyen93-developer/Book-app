9b8030307a1370bc5b3f9062855148522f6eec2f
Glitch (perfect-amber-power) <none>
2020-04-26 13:35:05 +0000

    update MVC for book app

--- routes/book.route.js
+++ routes/book.route.js
@@ -1,61 +1,21 @@
 var express = require('express');
-var shortid = require('shortid');
 
-var db = require('../db');
+var controllerBook = require('../controllers/book.controller');
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('books/index', {
-    books: db.get('books').value()
-  });
-});
+router.get('/', controllerBook.index);
 
-router.get('/search', function(req, res) {
-  var q = req.query.q;
-  var matchedBook = db.get('books').value().filter((book) => {
-    return book.title.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  });
-  
-  res.render('books/index', {
-    books: matchedBook
-  });
-});
+router.get('/search', controllerBook.search);
 
-router.get('/create', function(req, res) {
-  res.render('books/create');
-});
+router.get('/create', controllerBook.create);
 
-router.get('/:bookId/delete', function(req, res) {
-  var bookId = req.params.bookId;
-  
-  db.get('books').remove({ bookId: bookId }).write();
-  
-  res.redirect('/books');
-});
+router.get('/:bookId/delete', controllerBook.delete);
 
-router.get('/:bookId', function(req, res) {
-	var bookId = req.params.bookId;
+router.get('/:bookId', controllerBook.getUpdate);
 
-	var book = db.get('books').find({ bookId: bookId }).value();
+router.post('/create', controllerBook.postCreate);
 
-	res.render('books/update', {
-		book: book,
-    bookId: bookId
-	});
-});
-
-router.post('/create', function(req, res) {
-  req.body.bookId = shortid.generate();
-  db.get('books').push(req.body).write();
-  res.redirect('/books');
-});
-
-router.post('/update', function(req, res) {
-  var bookId = req.body.bookId;
-  
-  db.get('books').find({ bookId: bookId }).assign({ title: req.body.title }).write();
-  res.redirect('/books');
-});
+router.post('/update', controllerBook.postUpdate);
 
 module.exports = router
--- routes/transaction.route.js
+++ routes/transaction.route.js
@@ -1,39 +1,13 @@
 var express = require('express');
-var shortid = require('shortid');
 
-var db = require('../db');
+var controllerTransaction = require('../controllers/transaction.controller');
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  var data = db.get('transaction').value();
-  
-  var transactions = data.map(function (item) {
-    if (item.hasOwnProperty('userId') && item.hasOwnProperty('bookId')) {
-      var user = db.get('users').find({userId: item.userId}).value();
-      var book = db.get('books').find({bookId: item.bookId}).value();
-      
-      return {
-        user: user.name,
-        book: book.title
-      }
-    }
-  });
-  
-  res.render('transactions/index', {transactions});
-});
+router.get('/', controllerTransaction.index);
 
-router.get('/create', function(req, res) {
-  res.render('transactions/create', {
-    books: db.get('books').value(),
-    users: db.get('users').value()
-  });
-});
+router.get('/create', controllerTransaction.create);
 
-router.post('/create', function(req, res) {
-  req.body.tranId = shortid.generate();
-  db.get('transaction').push(req.body).write();
-  res.redirect('/transactions');
-});
+router.post('/create', controllerTransaction.postCreate);
 
 module.exports = router;

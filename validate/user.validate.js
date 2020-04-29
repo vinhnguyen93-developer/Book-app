a1232d68acb5e23e93c9a15d38cd8ad78c5f6c57
Glitch (breezy-gray-wishbone) <none>
2020-04-29 16:45:39 +0000

    add login method for book app

--- validate/user.validate.js
+++ validate/user.validate.js
@@ -1,12 +1,23 @@
+var db = require('../db');
+
 module.exports.postCreate = function(req, res, next) {
+  var email = db.get('users').find({ email: req.body.email }).value();
   var errors = [];
   
   if (!req.body.name) {
     errors.push('Name is required.');
   }
   
+  if (!req.body.email) {
+    errors.push('Email is required.');
+  }
+  
   if (req.body.name.length > 30) {
     errors.push('The name is too long.');
+  }
+  
+  if (email) {
+    errors.push('This email already exists.')
   }
   
   if (errors.length) {

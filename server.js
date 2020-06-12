24b52bfa4298a6fcc462226fee32f15d4a77f36f
Vinh Nguyen <developerthienkim@gmail.com>
2020-06-12 08:38:29 +0700

    Add router and use cors for api

--- server.js
+++ server.js
@@ -9,25 +9,20 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 mongoose.set("useUnifiedTopology", true);
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true});
 
-var userRoute = require('./routes/user.route');
-var bookRoute = require('./routes/book.route');
-var transactionRoute = require('./routes/transaction.route');
-var authRoute = require('./routes/auth.route');
-var cartRoute = require('./routes/cart.route');
-
 var apiAuthRoute = require('./api/routes/auth.route');
-var apiTransaction = require('./api/routes/transaction.route');
+var apiComment = require('./api/routes/comment.route');
 var apiUser = require('./api/routes/user.route');
-var apiBook = require('./api/routes/book.route');
-
-var authMiddleware = require('./middlewares/auth.middleware');
-var sessionMiddleware = require('./middlewares/session.middleware');
+var apiPost = require('./api/routes/post.route');
+//var apiReaction = require('./api/routes/reaction.route');
 
 var app = express();
+
+app.use(cors());
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -36,36 +31,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/auth', apiAuthRoute);
-app.use('/api/transactions', apiTransaction);
+app.use('/api/comments', apiComment);
 app.use('/api/users', apiUser);
-app.use('/api/books', apiBook);
+app.use('/api/posts', apiPost);
+//app.use('/api/reactions', apiReaction);
 
 app.use(cookieParser(process.env.SESSION_SECRET));
-app.use(sessionMiddleware);
+//app.use(sessionMiddleware);
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
-//book
-app.use('/books', bookRoute);
-
-//user
-app.use('/users', authMiddleware.requireAuth, userRoute);
-
-//transaction
-app.use('/transactions', authMiddleware.requireAuth, transactionRoute);
-
-//login
-app.use('/auth', authRoute);
-
-//cart
-app.use('/cart', cartRoute);
-
 // listen for requests :)
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT || 5000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
